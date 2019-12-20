745db8461219bef23f1f1c779b1ac8c2de121095
Josh Ganote <j.ganote0825@gmail.com>
2019-12-20 17:47:13 -0600

    created get router and sql query to join movies and genre table

---
+++ server/routes/movie.router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+router.get ('/', (req,res) => {
+    let queryText = 
+    `SELECT * FROM "movies"
+    JOIN "movies_genres" ON "movies"."id" = "movies_genres"."movies_id"
+    JOIN "genres" ON "movies_genres"."genres_id" = "genres"."id"
+    ORDER BY "movies"."id" ASC;`;
+    pool.query(queryText)
+    .then(results => {
+        res.send(results.rows);
+    })
+    .catch(error => {
+        console.log(`couldn't get movie`, error);
+        res.sendStatus(500);
+    })
+})
+module.exports = router;
