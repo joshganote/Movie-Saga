d508633a5a8ebd37ab74778df772a72a0fbf1eee
Josh Ganote <j.ganote0825@gmail.com>
2019-12-20 18:48:43 -0600

    created git call in index and mapped out an array of results in home.js

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,13 +1,35 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Home extends Component {
+    componentDidMount() {
+        this.props.dispatch({
+            type: 'GET_MOVIES'
+        });
+    }
 
-    render(){
+    render() {
+        const movieArray = this.props.store.movies.map((item, index) => {
+            return <tr key={index}>
+                <td><img alt="movie poster" src={item.poster}></img></td>
+                <td>
+                <h2>{item.title}</h2>
+                {item.description}
+                </td>
+                </tr>
+        })
 
-        return(
-            <div>HOME</div>
+        return (
+                <table className="App">
+                    <tbody>{movieArray}</tbody>
+                </table>
+           
         )
     }
 }
+// Makes our reducers available in our component
+const mapReduxStateToProps = (store) => ({
+    store
+});
 
-export default Home;
+export default connect(mapReduxStateToProps)(Home);
--- src/index.js
+++ src/index.js
@@ -4,17 +4,47 @@ import './index.css';
 import App from './components/App/App.js';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import axios from 'axios';
 // Provider allows us to use redux within our react app
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 // Import saga middleware
 import createSagaMiddleware from 'redux-saga';
-
+import { takeEvery, put } from 'redux-saga/effects';
 // Create the rootSaga generator function
 function* rootSaga() {
-
+    yield takeEvery('GET_MOVIES', getMovieSaga);
+    yield takeEvery('GET_DETAILS', getDetailSaga);
 }
 
+function* getMovieSaga(action){
+    try{
+        const response = yield axios({
+            method: 'GET',
+            url: '/api/movie'
+        });
+        yield put({
+            type: 'SET_MOVIES',
+            payload: response.data
+        });
+    } catch(err) {
+           console.log('error getting elements', err); 
+    }
+}
+function* getDetailSaga(action){
+    try{
+        const response = yield axios({
+            method: 'GET',
+            url: '/api/movie/detail'
+        });
+        yield put({
+            type: 'SET_MOVIES',
+            payload: response.data
+        });
+    } catch(err) {
+           console.log('error getting elements', err); 
+    }
+}
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
 
