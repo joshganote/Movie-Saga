1a8bf03fe7457b459090f6574fb497cb4a43240f
Josh Ganote <j.ganote0825@gmail.com>
2019-12-20 18:51:30 -0600

    listing out all detail data on page, need to work on the display

--- src/components/pages/Detail/Detail.js
+++ src/components/pages/Detail/Detail.js
@@ -1,13 +1,37 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Detail extends Component {
+    componentDidMount() {
+        this.props.dispatch({
+            type: 'GET_DETAILS'
+        });
+    }
 
-    render(){
+    render() {
+        const movieArray = this.props.store.movies.map((item, index) => {
+            return <tr key={index}>
+                <td>
+                <h2>{item.title}</h2>
+                {item.description}
+                {item.movies_id}
+                {item.genres_id}
+                {item.name}
+                </td>
+                </tr>
+        })
 
-        return(
-            <div>DETAIL</div>
+        return (
+                <table className="App">
+                    <tbody>{movieArray}</tbody>
+                </table>
+           
         )
     }
 }
+// Makes our reducers available in our component
+const mapReduxStateToProps = (store) => ({
+    store
+});
 
-export default Detail;
+export default connect(mapReduxStateToProps)(Detail);
