5644c14a962b54375c6ae350597669aaea45c1a0
Josh Ganote <j.ganote0825@gmail.com>
2019-12-20 21:26:30 -0600

    added input fields and button. Also brought in the genre array

--- src/components/pages/Edit/Edit.js
+++ src/components/pages/Edit/Edit.js
@@ -1,13 +1,64 @@
 import React, { Component} from 'react';
+import { connect } from 'react-redux';
 
 class Edit extends Component {
 
-    render(){
+    state = {
+        name: '',
+        description: ''
+    }
 
+    componentDidMount() {
+        this.props.dispatch({
+            type: 'GET_DETAILS'
+        });
+}
+
+handleChange = (event, inputKey) => {
+    this.setState({
+        
+        [inputKey]: event.target.value
+    })
+}
+
+   
+
+    render(){
+        const genresArray = this.props.store.genres.map((item, index) => {
+            return <tr key={index}>
+                <td>
+                    <h4>{item.name}</h4>
+                </td>
+            </tr>
+        })
         return(
+            <div>
             <div>EDIT</div>
+            <button>Cancel</button>
+            <button onSubmit={this.handleSubmit}>Save </button>
+            <br/>
+            <input
+                    type="text"
+                    placeholder="Genre"
+                    value={this.state.name}
+                    onChange={(event) => this.handleChange(event, 'name')}
+                />
+                <br/>
+                 <input
+                    type="text"
+                    placeholder="Description"
+                    value={this.state.description}
+                    onChange={(event) => this.handleChange(event, 'description')}
+                    required
+                />
+            {genresArray}
+            </div>
+    
         )
     }
 }
+const mapReduxStateToProps = (store) => ({
+    store
+});
 
-export default Edit;
+export default connect(mapReduxStateToProps)(Edit);
