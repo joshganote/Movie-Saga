992f0242a3c47f6f600c05d7b4cb5244006b7e38
Josh Ganote <j.ganote0825@gmail.com>
2019-12-20 18:50:33 -0600

    added a second get router for movie details (description, genre)

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -2,7 +2,22 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+
 router.get ('/', (req,res) => {
+    let queryText = 
+    `SELECT * FROM "movies"
+    ORDER BY "movies"."id" ASC;`;
+    pool.query(queryText)
+    .then(results => {
+        res.send(results.rows);
+    })
+    .catch(error => {
+        console.log(`couldn't get movie`, error);
+        res.sendStatus(500);
+    })
+})
+
+router.get ('/detail', (req,res) => {
     let queryText = 
     `SELECT * FROM "movies"
     JOIN "movies_genres" ON "movies"."id" = "movies_genres"."movies_id"
