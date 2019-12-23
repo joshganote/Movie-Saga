3024d34cd2ce9794d6403be5b53251c595b10d32
Josh Ganote <j.ganote0825@gmail.com>
2019-12-23 16:38:42 -0600

    had a post.router instead of a put

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -3,21 +3,34 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 
-router.get ('/', (req,res) => {
-    let queryText = 
-    `SELECT * FROM "movies"
+router.get('/', (req, res) => {
+    let queryText =
+        `SELECT * FROM "movies"
     JOIN "movies_genres" ON "movies"."id" = "movies_genres"."movies_id"
     JOIN "genres" ON "movies_genres"."genres_id" = "genres"."id"
     ORDER BY "movies"."title" ASC;`;
     pool.query(queryText)
-    .then(results => {
-        res.send(results.rows);
-    })
-    .catch(error => {
-        console.log(`couldn't get movie`, error);
-        res.sendStatus(500);
-    })
+        .then(results => {
+            res.send(results.rows);
+        })
+        .catch(error => {
+            console.log(`couldn't get movie`, error);
+            res.sendStatus(500);
+        })
 })
+
+router.put('/', (req, res) => {
+    const newDetail = req.body;
+    const queryText = `UPDATE "movies" SET "title"=$1, "description"=$2;`;
+
+    pool.query(queryText, [newDetail.name, newDetail.description])
+        .then(() => { res.sendStatus(201); })
+        .catch((er) => {
+            console.log('error posting new detail', err);
+            res.sendStatus(500);
+        });
+});
+
 module.exports = router;
 
 // router.get ('/', (req,res) => {
